672c0c1a62cde47a43b9c9d074d0906e6fca6837
Coco Zeng <64930215+superasymmetry@users.noreply.github.com>
2025-02-15 18:02:28 -0500

    Update core.py

--- core.py
+++ core.py
@@ -21,6 +21,51 @@ processor = WhisperProcessor.from_pretrained("openai/whisper-small")
 model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-small")
 model.config.forced_decoder_ids = None
 
+def choose_word():
+    with open("wordlib.json", "r") as file:
+        data = json.load(file)
+
+    data["words"].sort(key=lambda x: x["score"])
+    print(data["words"])
+    return [word["word"] for word in data["words"]]
+
+def generate():
+    words = choose_word()
+    ran = random.randint(0, 9)
+    w = words[ran]
+    print(call(w))
+
+def call(w):
+    basedir = os.path.dirname(os.path.abspath(__file__))
+    folder_path = os.path.join(basedir,'config.json')
+    with open((folder_path), "rb") as config_file:
+        config = json.load(config_file)
+    api_key = config["api_key"]
+    print(api_key)
+    client = Groq(
+        api_key = api_key,
+    )
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": "you are a speech therapist for a child of age 6."
+            },
+            {
+                "role": "user",
+                "content": f"Please generate one short sentence for a child of age 6 to read with the following word: {w}",
+            }
+        ],
+
+        # The language model which will generate the completion.
+        model="llama-3.1-8b-instant",
+        stop=None,
+        stream=False,
+    )
+
+    # Print the completion returned by the LLM.
+    print(chat_completion.choices[0].message.content)
+
 def transcription_func(audio_data):
     device = "cuda" if torch.cuda.is_available() else "cpu"
     asr_pipeline = pipeline("automatic-speech-recognition", model="openai/whisper-small", device=device)
