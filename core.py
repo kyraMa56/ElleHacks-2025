31bebdc70b01cea20a8c7df504e510d0fe51cdb1
Maria Natnaree <marianatnaree@gmail.com>
2025-02-15 14:43:45 -0500

    updating the real-time transcription function

--- core.py
+++ core.py
@@ -1,8 +1,12 @@
 import json
-from transformers import WhisperProcessor, WhisperForConditionalGeneration
+import time
+import torch
+import pyaudio
+import numpy as np
+import wave 
+from transformers import WhisperProcessor, WhisperForConditionalGeneration, pipeline
 from datasets import load_dataset
 from huggingface_hub import InferenceClient
-import torch
 from evaluate import load
 from transformers import AutoProcessor, AutoModelForSpeechSeq2Seq
 
@@ -11,13 +15,14 @@ with open('hf_api_key.json', 'r') as file:
 API_TOKEN = data["HF_API_KEY"]
 
 inference = InferenceClient(model="openai/whisper-small", token=API_TOKEN)
-processor = AutoProcessor.from_pretrained("stringbot/whisper-small-hi")
-model = AutoModelForSpeechSeq2Seq.from_pretrained("stringbot/whisper-small-hi")
-# processor = WhisperProcessor.from_pretrained("openai/whisper-small")
-# model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-small")
+# processor = AutoProcessor.from_pretrained("stringbot/whisper-small-hi")
+# model = AutoModelForSpeechSeq2Seq.from_pretrained("stringbot/whisper-small-hi")
+processor = WhisperProcessor.from_pretrained("openai/whisper-small")
+model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-small")
 model.config.forced_decoder_ids = None
 
 def transcription():
+    # Taking audio input from user
     ds = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation", trust_remote_code=True)
     sample = ds[0]["audio"]
     input_features = processor(sample["array"], sampling_rate=sample["sampling_rate"], return_tensors="pt").input_features 
@@ -31,6 +36,48 @@ def transcription():
     
     return transcription
 
+def real_time_transcription(press_record=False):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    asr_pipeline = pipeline("automatic-speech-recognition", model="openai/whisper-small", device=device)
+
+    # when user press record
+    if press_record:
+        # Audio Settings
+        FORMAT = pyaudio.paInt16  # 16-bit format
+        CHANNELS = 1  # Mono
+        RATE = 16000  # Sample rate
+        CHUNK = 1024  # Buffer size
+        RECORD_SECONDS = 5  
+        TOTAL_DURATION = 10
+
+        p = pyaudio.PyAudio()
+        stream = p.open(format=FORMAT, channels=CHANNELS,
+                        rate=RATE, input=True,
+                        frames_per_buffer=CHUNK)
+
+        print("Listening...")
+        start_time = time.time()
+        while time.time() - start_time < TOTAL_DURATION:
+            frames = []
+            
+            # Record short audio chunks
+            for _ in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+                data = stream.read(CHUNK, exception_on_overflow=False)
+                frames.append(data)
+            
+            # Convert audio to numpy array
+            audio_data = np.frombuffer(b''.join(frames), dtype=np.int16).astype(np.float32) / 32768.0
+            
+            transcription = asr_pipeline(audio_data)
+            print("Transcription:", transcription["text"])
+            
+    p.terminate()
+    stream.stop_stream()
+    stream.close()
+    print('End transcription')
+    
+    return transcription["text"]
+
 def map_to_pred(batch):
     audio = batch["audio"]
     input_features = processor(audio["array"], sampling_rate=audio["sampling_rate"], return_tensors="pt").input_features
@@ -42,18 +89,20 @@ def map_to_pred(batch):
     batch["prediction"] = processor.tokenizer._normalize(transcription)
     return batch
 
-def evaluation(output):
-    librispeech_test_clean = load_dataset("librispeech_asr", "clean", split="test")
-    result = librispeech_test_clean.map(map_to_pred)
-
+def evaluation():
+    # Create new testing dataset/references
+    user_input =  [real_time_transcription(press_record=True)]
+    references = ['I have four pencils']
+    
     wer = load("wer")
-    score = 100 * wer.compute(references=result["reference"], predictions=result["prediction"])
-
+    score = 100 * wer.compute(references=references, predictions=user_input)
+    
     return score
 
 if __name__ == '__main__':
     '''
     [' Mr. Quilter is the apostle of the middle classes, and we are glad to welcome his gospel.']
     '''
-    print(transcription())
+    print(evaluation())
     
+
